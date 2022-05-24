3e9af014fe5943f5f2cdbdfb1facb6dafee8921a
ongimkshp <doducmanh.bb@gmail.com>
2022-05-24 23:49:16 +0700

    giai thuat

--- test.js
+++ test.js
@@ -1,30 +1,55 @@
-const arr = [1, 2, 1, 7, 1, 1, 2, 2, 1, 2, 3, 1, 9, 12, 1, 3, 40];
+// Ex1
+/*const arr = ['a', 'a', 'cd', 'cd', 'def', 'gh', 'a', 'def', 'c'];
 
-function unique(arr){
-    var newArr = [];
-    for(var i  = 0; i < arr.length; i++ ){
-        if(!newArr.includes(arr[i])){
-            newArr.push(arr[i]);
+const  sortString = (a, b) => {
+    if( a > b)
+        return 1;
+    else if( a < b)
+        return -1;
+    else
+        return 0;
+}
+
+const countAppear = (arr) =>{
+    let newArr = [];
+    let initString = arr[0];
+    let count = 0;
+    for(let i = 0; i <= arr.length ; i++){
+        if(initString !== arr[i]){
+            newArr.push({
+                string: initString,
+                count: count
+            })
+            initString = arr[i]
+            count = 0;
         }
+        count++;
     }
     return newArr;
-}
-function check(arr, shortArr){
-    let length;
-    let newArr = [];
-    for(let i = 0; i < shortArr.length; i++){
-        length = arr.filter(item => item === shortArr[i]).length
-        if(length % 2 === 1){
-            newArr.push(shortArr[i]);
-        }
+};
+
+const newArr = countAppear(arr.sort(sortString));
+
+// Max count
+newArr.forEach(item => {
+    let i = 0;
+    if(item.count > i) {
+        i = item.count;
     }
-    return newArr.sort(equal);
-}
+})
+var maxCount = Math.max(...newArr.map(item => item.count));
+var maxLength = newArr.find(item => item.count === maxCount).string.length;
 
-function equal(a, b){
-    if (a > b) return 1;
-    if (a < b) return -1;
-    return 0;
-}
-console.log(arr.sort(equal));
+console.log(maxLength);*/
+// Ex2
+const arr = [1, 9, 4, 2, 6, 8, 12];
+var total = 0;
 
+const sum = arr.reduce((init, current) => {
+    if(current == 2){
+        total = init
+    }
+    return init = init + current;
+}, 0)
+
+console.log(total);
